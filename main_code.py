2ceb49fa75dfc4a4afc24e02592a4a2fc5509f90
sreerampeela <59723039+sreerampeela@users.noreply.github.com>
2022-08-26 15:01:16 +0530

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -2,29 +2,25 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+# from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import pickle
 # reading and cleaning data
 data = pd.read_csv("houseprice.csv")
-# print(data.head())
-# print(data.shape)
-# print(data.describe())
-# print(data.isnull().sum())
-le = LabelEncoder()
-sting_var = ['mainroad','guestroom','basement','hotwaterheating','airconditioning',
-                                                                 'prefarea','furnishingstatus']
-for i in sting_var:
-    labels = le.fit_transform(data[i])
-# print(labels)
-    data.drop(i,axis=1,inplace=True)
-    data[i] = labels
-# print(data.furnishingstatus)
-# print(data.ndim)
-# print(data.mainroad.ndim)
-variables = data.columns
-x = data.drop('price',axis=1)
-y = data['price']
+for i in list(data.columns):
+    data[i].fillna(0, inplace=True)
+# print(data)
+for i in ['mainroad','guestroom','basement','hotwaterheating','airconditioning','prefarea','furnishingstatus']:
+    p = pd.get_dummies(data[i],prefix=i)
+    # data.drop(i)
+    # data.append(x)
+    data=pd.concat([p,data],axis=1)
+# print(data)
+data_cleaned=data.drop(['mainroad','guestroom','basement','hotwaterheating','airconditioning','prefarea','furnishingstatus'],axis=1)
+print(data_cleaned)
+# data = data.drop('mainroad')
+x = data_cleaned.drop('price',axis=1)
+y = data_cleaned['price']
 print(x.ndim)
 print(y.ndim)
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
@@ -34,5 +30,5 @@ predictions = model.predict(x_test)
 print('mean_squared_error : ', mean_squared_error(y_test, predictions))
 print('mean_absolute_error : ', mean_absolute_error(y_test, predictions))
 
-# creating a model pickle file
+#creating a model pickle file
 pickle.dump(model,open('houseprice.pkl','wb'))
